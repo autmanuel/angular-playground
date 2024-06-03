7ec02df5d01bc11757e26d256c7bbcf1f56566a1
Stefan Kerschbaumer <xsip@pm.me>
2024-06-03 23:30:04 +0200

    adds language switcher

--- src/app/ui/header/header.component.ts
+++ src/app/ui/header/header.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import {RouterLink, RouterLinkActive} from "@angular/router";
+import {TranslateService} from "@ngx-translate/core";
 
 @Component({
   selector: 'app-header',
@@ -10,17 +11,39 @@ import {RouterLink, RouterLinkActive} from "@angular/router";
   ],
   template: `
     <nav class="w-full bg-slate-800 text-white p-2.5 font-bold drop-shadow-md h-12">
-      <div class="flex gap-5">
-        <p class="cursor-pointer hover:scale-125 transition-all" routerLinkActive="underline" routerLink="/home">Home</p>
-        <p class="cursor-pointer hover:scale-125 transition-all" routerLinkActive="underline" routerLink="/to-do">ToDo</p>
-        <p class="cursor-pointer hover:scale-125 transition-all" routerLinkActive="underline" routerLink="/tests">Snippets</p>
-        <p class="cursor-pointer hover:scale-125 transition-all" routerLinkActive="underline" routerLink="/employees">Employees</p>
-        <p class="cursor-pointer hover:scale-125 transition-all" routerLinkActive="underline" routerLink="/weather">Weather</p>
+      <div class="flex gap-5 justify-between">
+        <div class="flex gap-5">
+          <p class="cursor-pointer hover:scale-125 transition-all" routerLinkActive="underline" routerLink="/home">
+            Home</p>
+          <p class="cursor-pointer hover:scale-125 transition-all" routerLinkActive="underline" routerLink="/to-do">
+            ToDo</p>
+          <p class="cursor-pointer hover:scale-125 transition-all" routerLinkActive="underline" routerLink="/tests">
+            Snippets</p>
+          <p class="cursor-pointer hover:scale-125 transition-all" routerLinkActive="underline" routerLink="/employees">
+            Employees</p>
+          <p class="cursor-pointer hover:scale-125 transition-all" routerLinkActive="underline" routerLink="/weather">
+            Weather</p>
+        </div>
+        <div class="px-2 flex gap-1">
+          <div (click)="setLanguage('en')" [class.bg-red-400]="currentLanguage === 'en'" class="transition-all ease-in-out duration-300 cursor-pointer border px-2 hover:scale-110" ><p>EN</p></div>
+          <div (click)="setLanguage('de')" [class.bg-red-400]="currentLanguage === 'de'" class="transition-all ease-in-out duration-300 cursor-pointer  border px-2 hover:scale-110"><p>DE</p></div>
+        </div>
       </div>
     </nav>
   `,
   styles: ``
 })
-export class HeaderComponent {
+export class HeaderComponent  implements OnInit {
+  currentLanguage = localStorage.getItem('language') ?? 'en';
+  translateService = inject(TranslateService);
+  setLanguage(language: 'de' | 'en') {
+    localStorage.setItem('language', language);
+    this.currentLanguage = localStorage.getItem('language') ?? 'en';
+    this.translateService.setDefaultLang(this.currentLanguage);
+  }
 
+  ngOnInit() {
+    this.currentLanguage = localStorage.getItem('language') ?? 'en';
+    this.translateService.setDefaultLang(this.currentLanguage);
+  }
 }
