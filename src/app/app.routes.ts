f794b7fd6b1dd76cc4772892d5a3e51ca4f7ee06
Manuel H <manuel.hirschbeck@gmail.com>
2024-06-03 22:01:34 +0200

    commented calculator.coponent.spec.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,7 @@ import {ToDoComponent} from "./routes/to-do/to-do.component";
 import {TestsComponent} from "./tests/tests.component";
 import {WeatherComponent} from "./routes/weather/weather.component";
 import {EmployeesComponent} from "./routes/employees/employees.component";
-import {CalculatorComponent} from "./routes/calculator/calculator.component";
+// import {CalculatorComponent} from "./routes/calculator/calculator.component";
 
 export const routes: Routes = [
   {path:'', pathMatch: 'full', redirectTo: 'home'},
@@ -12,5 +12,5 @@ export const routes: Routes = [
   {path:'to-do', component: ToDoComponent},
   {path:'tests', component: TestsComponent},
   {path:'employees', component: EmployeesComponent},
-  {path:'calculator', component: CalculatorComponent}
+  // {path:'calculator', component: CalculatorComponent}
 ];
