05ac9de3a31708b056a3527b35644c9347a9282d
Manuel H <manuel.hirschbeck@gmail.com>
2024-06-03 21:57:38 +0200

    created weather.component.ts

--- src/app/routes/weather/weather.component.ts
+++ src/app/routes/weather/weather.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {CustomWeatherModel} from "./weather.model";
 
 @Component({
   selector: 'app-weather',
@@ -11,6 +13,16 @@ import { Component } from '@angular/core';
   `,
   styles: ``
 })
-export class WeatherComponent {
 
+
+export class WeatherComponent implements OnInit{
+  httpClient = inject(HttpClient);
+  weatherResponse? : CustomWeatherModel;
+
+  ngOnInit() {
+    this.httpClient.get<CustomWeatherModel>('/api/weather').subscribe(response => {
+
+    })
 }
+}
+
