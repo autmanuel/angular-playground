241afdfd5ae9b118b44fd00bc9c976ac28f8f550
Manuel H <manuel.hirschbeck@gmail.com>
2024-06-06 22:41:46 +0200

    add example for ng-content projection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,25 @@ import { RouterOutlet } from '@angular/router';
 import {AsyncPipe, NgIf} from "@angular/common";
 import {StateService} from "./state.service";
 import {HeaderComponent} from "./ui/header/header.component";
+import {CardComponent} from "./ui/card/card.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgIf, AsyncPipe, HeaderComponent],
+  imports: [RouterOutlet, NgIf, AsyncPipe, HeaderComponent, CardComponent],
   template: `
     <app-header></app-header>
     <router-outlet />
+<!--    <app-card [shouldBeBlack]="true">-->
+<!--      <h1 title class="bg-slate-800 text-green-800">test</h1>-->
+<!--      <h1 description>wwefewfweewfwf</h1>-->
+
+<!--    </app-card>-->
+<!--    <app-card [shouldBeBlack]="false">-->
+<!--      <h1 title class="bg-slate-800 text-green-800">test</h1>-->
+<!--      <h1 description>wwefewfweewfwf</h1>-->
+<!--    -->
+<!--    </app-card>-->
   `,
   styles: [``],
 })
--- src/app/ui/card/card.component.ts
+++ src/app/ui/card/card.component.ts
@@ -1,16 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, Input, input} from '@angular/core';
 
 @Component({
   selector: 'app-card',
   standalone: true,
   imports: [],
   template: `
-    <p>
-      card works!
-    </p>
+
+    <div class="m-5 p-5 {{shouldBeBlack() ? 'bg-black': ''}} text-center w-[500px] h-[600px] flex bg-slate-800 gap-5">
+
+      <div class="w-full p-5">
+        <div class="mt-5">
+          <ng-content select="[title]"></ng-content>
+        </div>
+        <div class="mt-5">
+          <ng-content select="[description]"></ng-content>
+        </div>
+      </div>
+    </div>
   `,
   styles: ``
 })
 export class CardComponent {
-
+  shouldBeBlack = input<boolean>();
 }
